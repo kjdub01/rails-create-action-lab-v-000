e7b23cd4ba5f031919aae83c9f5fd9c232fc699d
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-10 04:25:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new 
     @student.first_name(params[:student][:first_name])
+    @student.first_name(params[:student][:first_name])
   end
 
 end
